a6537481b2d01dc63759b0c7250df73a4f83534f
Katelynn <grayspence2@hotmail.com>
2023-10-17 18:47:05 -0400

    added the ability to exit out of the game with ctrl + C

--- play.js
+++ play.js
@@ -1,1 +1,16 @@
 const connect = require('./client.js');
+
+// setup interface to handle user input from stdin
+const handleUserInput = function () {
+  if (data = '\u0003') {
+    process.exit();
+  }
+};
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
